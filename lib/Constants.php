703f3e6f39b1e17e9c1135ddca7fbae7e89c2de0
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-10-28 00:16:32 +0200

    Constants.php, for avoiding explicit literals.

---
+++ lib/Constants.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2022 Claus-Justus Heine
+ * @license   AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\FilesArchive;
+
+/** A couple of constants in order to avoid sting literals. */
+class Constants
+{
+  public const PATH_SEPARATOR = '/';
+}
