1d70aee654562d76722b7249310e2f7786d249df
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-11-04 08:25:01 +0100

    MountProvider, fiddle with moveable mounts.

--- lib/Mount/MountProvider.php
+++ lib/Mount/MountProvider.php
@@ -7,17 +7,21 @@ use Throwable;
 
 // F I X M E internal
 use OC\Files\Mount\MountPoint;
+use OC\Files\Mount\MoveableMount;
 
 use Psr\Log\LoggerInterface;
 
+use OCP\Files\Mount\IMountManager;
 use OCP\Files\Config\IMountProvider;
 use OCP\Files\Storage\IStorageFactory;
 use OCP\Files\FileInfo;
+use OCP\Files\Folder;
 use OCP\Files\NotFoundException as FileNotFoundException;
 use OCP\Files\IRootFolder;
 use OCP\IUser;
 use OCP\IL10N;
 use OCP\AppFramework\IAppContainer;
+use OCP\Lock\ILockingProvider as Locks;
 
 use OCA\FilesArchive\Db\ArchiveMount;
 use OCA\FilesArchive\Db\ArchiveMountMapper;
@@ -47,6 +51,9 @@ class MountProvider implements IMountProvider
   /** @var IRootFolder */
   private $rootFolder;
 
+  /** @var IMountManager */
+  private $mountManager;
+
   /** @var IL10N */
   private $l;
 
@@ -57,6 +64,7 @@ class MountProvider implements IMountProvider
     IL10N $l10n,
     IAppContainer $appContainer,
     IRootFolder $rootFolder,
+    IMountManager $mountManager,
     ArchiveMountMapper $mountMapper,
   ) {
     $this->appName = $appName;
@@ -64,6 +72,7 @@ class MountProvider implements IMountProvider
     $this->l = $l10n;
     $this->appContainer = $appContainer;
     $this->rootFolder = $rootFolder;
+    $this->mountManager = $mountManager;
     $this->mountMapper = $mountMapper;
   }
   // phpcs:enable
@@ -99,6 +108,8 @@ class MountProvider implements IMountProvider
       return [];
     }
 
+    // $this->logInfo('USERFOLDER ' . $userFolder->getPath() . ' INTERNAL ' . $userFolder->getInternalPath());
+
     $mounts = [];
     $mountMapping = $this->mountMapper->findAll($userId);
 
@@ -126,8 +137,6 @@ class MountProvider implements IMountProvider
         continue;
       }
 
-      $mountPath = $mountFolder->getPath();
-
       $storage = new ArchiveStorage([
         'appContainer' => $this->appContainer,
         'archiveFile' => $archiveFile,
@@ -135,22 +144,116 @@ class MountProvider implements IMountProvider
 
       $mounts[] = new class(
         $storage,
-        $mountPath,
-        null,
+        $userFolder,
+        $mountFolder,
         $loader,
-        [
-          'filesystem_check_changes' => 1,
-          'readonly' => true,
-          'previews' => true,
-          'enable_sharing' => false, // cannot work, mount needs DB access
-          'authenticated' => true,
-        ]
-      ) extends MountPoint
+        $this->mountManager,
+        $this->mountMapper,
+        $mount,
+        $this->logger,
+      ) extends MountPoint implements MoveableMount
       {
+        use \OCA\FilesArchive\Traits\LoggerTrait;
+
+        /** @var IMountManager */
+        private $mountManager;
+
+        /** @var ArchiveMountMapper */
+        private $mountMapper;
+
+        /** @var ArchiveMount */
+        private $mountEntity;
+
+        /** @var Folder */
+        private $userFolder;
+
+        /** @var Folder */
+        private $mountFolder;
+
+        /**
+         * @param ArchiveStorage $storage
+         *
+         * @param Folder $userFolder
+         *
+         * @param Folder $mountFolder
+         *
+         * @param IStorageFactory $loader
+         *
+         * @param IMountManager $mountManager
+         *
+         * @param ArchiveMountMapper $mountMapper
+         *
+         * @param ArchiveMount $mountEntity
+         *
+         * @param LoggerInterface $logger
+         */
+        public function __construct(
+          ArchiveStorage $storage,
+          Folder $userFolder,
+          Folder $mountFolder,
+          IStorageFactory $loader,
+          IMountManager $mountManager,
+          ArchiveMountMapper $mountMapper,
+          ArchiveMount $mountEntity,
+          LoggerInterface $logger,
+        ) {
+          parent::__construct(
+            storage: $storage,
+            mountpoint: $mountFolder->getPath(),
+            loader: $loader,
+            mountOptions: [
+              'filesystem_check_changes' => 1,
+              'readonly' => true,
+              'previews' => true,
+              'enable_sharing' => false, // cannot work, mount needs DB access
+              'authenticated' => true,
+            ]
+          );
+          $this->mountManager = $mountManager;
+          $this->mountMapper = $mountMapper;
+          $this->mountEntity = $mountEntity;
+          $this->userFolder = $userFolder;
+          $this->mountFolder = $mountFolder;
+          $this->logger = $logger;
+        }
+
         /** {@inheritdoc} */
         public function getMountType()
         {
           return 'external'; // Constants::APP_NAME;
+        }
+
+        /** {@inheritdoc} */
+        public function moveMount($target)
+        {
+          $userFolderPath = $this->userFolder->getPath();
+          if (!str_starts_with($target, $userFolderPath)) {
+            return false;
+          }
+          $relativeTarget = substr($target, strlen($userFolderPath));
+
+          $this->mountEntity->setMountPointPath($relativeTarget);
+          $this->mountEntity->setMountPointPathHash(md5($relativeTarget));
+          $this->mountMapper->update($this->mountEntity);
+
+          $this->mountFolder->unlock(Locks::LOCK_EXCLUSIVE);
+          $this->mountFolder->move($target);
+          $this->mountFolder->lock(Locks::LOCK_EXCLUSIVE);
+
+          return true;
+        }
+
+        /** {@inheritdoc} */
+        public function removeMount()
+        {
+          $this->mountMapper->delete($this->mountEntity);
+          $this->mountManager->removeMount($this->mountFolder->getPath());
+
+          // This is cludgy. I wonder what was the intended handling ...
+          $this->mountFolder->unlock(Locks::LOCK_EXCLUSIVE);
+          $this->mountFolder->delete();
+          $this->mountFolder->lock(Locks::LOCK_EXCLUSIVE);
+          return true;
         }
       };
     }
