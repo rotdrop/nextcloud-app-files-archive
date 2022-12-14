bc418469074f453aa0b1e700968ecf71530ea4cc
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-12-14 08:11:51 +0100

    git subrepo pull src/toolkit
    subrepo:
      subdir:   "src/toolkit"
      merged:   "6436f80"
    upstream:
      origin:   "git@github.com:rotdrop/nextcloud-app-toolkit-js.git"
      branch:   "main"
      commit:   "6436f80"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

--- src/toolkit/util/pangram.js
+++ src/toolkit/util/pangram.js
@@ -20,7 +20,8 @@
 import { appName } from '../../config.js';
 
 // TRANSLATORS: This should be a pangram in the target language, please have a look at
-// TRANSLATORS: https://en.wikipedia.org/wiki/Pangramwhere you can also find examples for some languages.
+// TRANSLATORS: https://en.wikipedia.org/wiki/Pangram where you can
+// TRANSLATORS: also find examples for some languages.
 const pangram = t(appName, 'The quick brown fox jumps over the lazy dog.');
 
 export default pangram;
