ad83e6852b1dacde583729a7bc03cd1bb7db1b36
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-12 22:11:52 +0100

    Fix copyright notice to use xml comments.
    Duplicated <script> tags are not tolerated by eslint. Sad story.

--- src/PersonalSettings.vue
+++ src/PersonalSettings.vue
@@ -1,42 +1,43 @@
-<script>
-/**
- * @copyright Copyright (c) 2022, 2023 Claus-Justus Heine <himself@claus-justus-heine.de>
- * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @license AGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-</script>
+<!--
+ - @copyright Copyright (c) 2022-2024 Claus-Justus Heine <himself@claus-justus-heine.de>
+ - @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ - @license AGPL-3.0-or-later
+ -
+ - This program is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU Affero General Public License as
+ - published by the Free Software Foundation, either version 3 of the
+ - License, or (at your option) any later version.
+ -
+ - This program is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU Affero General Public License for more details.
+ -
+ - You should have received a copy of the GNU Affero General Public License
+ - along with this program. If not, see <http://www.gnu.org/licenses/>.
+ -->
 <template>
-  <SettingsSection :class="[...cloudVersionClasses, appName]" :title="t(appName, 'Archive Manager, Personal Settings')">
-    <AppSettingsSection :title="t(appName, 'Security Options')">
-      <SettingsInputText v-model="humanArchiveSizeLimit"
-                         :label="t(appName, 'Archive Size Limit')"
-                         :hint="t(appName, 'Disallow archive extraction for archives with decompressed size larger than this limit.')"
-                         :disabled="loading"
-                         @update="saveTextInput(...arguments, 'archiveSizeLimit')"
+  <div :class="['templateroot', appName, ...cloudVersionClasses]">
+    <h1 class="title">
+      t(appName, 'Archive Manager, Personal Settings')
+    </h1>
+    <NcSettingsSection :name="t(appName, 'Security Options')">
+      <TextField :value.sync="humanArchiveSizeLimit"
+                 :label="t(appName, 'Archive Size Limit')"
+                 :hint="t(appName, 'Disallow archive extraction for archives with decompressed size larger than this limit.')"
+                 :disabled="loading"
+                 @submit="saveTextInput('archiveSizeLimit')"
       />
       <span v-if="archiveSizeLimitAdmin > 0" :class="{ hint: true, 'admin-limit-exceeded': archiveSizeLimitAdmin < archiveSizeLimit, 'icon-error': archiveSizeLimitAdmin < archiveSizeLimit }">
         {{ t(appName, 'Administrative size limit: {value}', { value: humanArchiveSizeLimitAdmin }) }}
       </span>
-    </AppSettingsSection>
-    <AppSettingsSection :title="t(appName, 'Mount Options')">
-      <SettingsInputText v-model="mountPointTemplate"
-                         :label="t(appName, 'Template for the default name of the mount point')"
-                         :hint="t(appName, '{archiveFileName} will be replaced by the filename of the archive file without extensions.')"
-                         placeholder="{archiveFileName}"
-                         @update="saveTextInput(...arguments, 'mountPointTemplate')"
+    </NcSettingsSection>
+    <NcSettingsSection :name="t(appName, 'Mount Options')">
+      <TextField :value.sync="mountPointTemplate"
+                 :label="t(appName, 'Template for the default name of the mount point')"
+                 :hint="t(appName, '{archiveFileName} will be replaced by the filename of the archive file without extensions.')"
+                 placeholder="{archiveFileName}"
+                 @submit="saveTextInput('mountPointTemplate')"
       />
       <div class="settings-option">
         <input :id="id + '-mount-strip-common-prefix'"
@@ -71,13 +72,13 @@
           {{ t(appName, 'default to scheduling mount requests as background job') }}
         </label>
       </div>
-    </AppSettingsSection>
-    <AppSettingsSection :title="t(appName, 'Extraction Options')">
-      <SettingsInputText v-model="extractTargetTemplate"
-                         :label="t(appName, 'Template for the default name of the extraction folder')"
-                         :hint="t(appName, '{archiveFileName} will be replaced by the filename of the archive file without extensions.')"
-                         placeholder="{archiveFileName}"
-                         @update="saveTextInput(...arguments, 'extractTargetTemplate')"
+    </NcSettingsSection>
+    <NcSettingsSection :name="t(appName, 'Extraction Options')">
+      <TextField :value.sync="extractTargetTemplate"
+                 :label="t(appName, 'Template for the default name of the extraction folder')"
+                 :hint="t(appName, '{archiveFileName} will be replaced by the filename of the archive file without extensions.')"
+                 placeholder="{archiveFileName}"
+                 @submit="saveTextInput(...arguments, 'extractTargetTemplate')"
       />
       <div class="settings-option">
         <input :id="id + '-extract-strip-common-prefix'"
@@ -112,28 +113,27 @@
           {{ t(appName, 'default to scheduling extraction requests as background job') }}
         </label>
       </div>
-    </AppSettingsSection>
-  </SettingsSection>
+    </NcSettingsSection>
+  </div>
 </template>
 
 <script>
-import { appName } from './config.js'
-import SettingsInputText from '@rotdrop/nextcloud-vue-components/lib/components/SettingsInputText'
-import AppSettingsSection from '@nextcloud/vue/dist/Components/NcAppSettingsSection'
-import SettingsSection from '@nextcloud/vue/dist/Components/NcSettingsSection'
-import { generateUrl } from '@nextcloud/router'
-import { showError, showSuccess, showInfo, TOAST_DEFAULT_TIMEOUT, TOAST_PERMANENT_TIMEOUT } from '@nextcloud/dialogs'
-import axios from '@nextcloud/axios'
-import settingsSync from './toolkit/mixins/settings-sync'
+import {
+  NcSettingsSection,
+} from '@nextcloud/vue'
+import TextField from '@rotdrop/nextcloud-vue-components/lib/components/TextFieldWithSubmitButton.vue'
+import settingsSync from './toolkit/mixins/settings-sync.js'
 import cloudVersionClasses from './toolkit/util/cloud-version-classes.js'
 
 export default {
   name: 'PersonalSettings',
   components: {
-    AppSettingsSection,
-    SettingsSection,
-    SettingsInputText,
+    NcSettingsSection,
+    TextField,
   },
+  mixins: [
+    settingsSync,
+  ],
   data() {
     return {
       cloudVersionClasses,
@@ -153,9 +153,6 @@ export default {
       loading: true,
     }
   },
-  mixins: [
-    settingsSync,
-  ],
   watch: {
   },
   created() {
@@ -166,11 +163,14 @@ export default {
   methods: {
     async getData() {
       // slurp in all personal settings
-      this.fetchSettings('personal');
+      this.fetchSettings('personal')
       this.loading = false
     },
-    async saveTextInput(value, settingsKey, force) {
-      return this.saveConfirmedSetting(value, 'personal', settingsKey, force);
+    async saveTextInput(settingsKey, value, force) {
+      if (value === undefined) {
+        value = this[settingsKey] || ''
+      }
+      return this.saveConfirmedSetting(value, 'personal', settingsKey, force)
     },
     async saveSetting(setting) {
       return this.saveSimpleSetting(setting, 'personal')
@@ -185,11 +185,14 @@ export default {
     --cloud-theme-filter: var(--background-invert-if-dark);
   }
 }
-.settings-section {
-  :deep(.settings-section__title) {
+.templateroot::v-deep {
+  h1.title {
+    margin: 30px 30px 0px;
+    font-size:revert;
+    font-weight:revert;
+    position: relative;
     padding-left:60px;
     height:32px;
-    position: relative;
     &::before {
       content: "";
       position: absolute;
@@ -205,33 +208,28 @@ export default {
       filter: var(--cloud-theme-filter);
     }
   }
-  .app-settings-section {
-    .settings-option {
-      margin:0.5ex 0;
-    }
-    .flex-container {
-      display:flex;
+  .flex-container {
+    display:flex;
       &.flex-center {
         align-items:center;
       }
-    }
-    .label-container {
-      height:34px;
-      display:flex;
-      align-items:center;
-      justify-content:left;
-    }
-    .hint {
-      color: var(--color-text-lighter);
-      font-size: 80%;
-      &.admin-limit-exceeded {
-        color:red;
-        font-weight:bold;
-        font-style:italic;
-        &.icon-error {
-          padding-left:20px;
-          background-position:left;
-        }
+  }
+  .label-container {
+    height:34px;
+    display:flex;
+    align-items:center;
+    justify-content:left;
+  }
+  .hint {
+    color: var(--color-text-lighter);
+    font-size: 80%;
+    &.admin-limit-exceeded {
+      color:red;
+      font-weight:bold;
+      font-style:italic;
+      &.icon-error {
+        padding-left:20px;
+        background-position:left;
       }
     }
   }
