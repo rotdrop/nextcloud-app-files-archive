9d202a15e55dada9d377a66fe238a66aa74a8dcf
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-13 10:18:08 +0100

    Handle unreachable ("dead") JS code.

--- webpack.config.js
+++ webpack.config.js
@@ -75,9 +75,15 @@ webpackConfig.plugins = webpackConfig.plugins.concat([
       'style/**/*.scss',
     ],
     exclude: [
+      'src/toolkit/util/ajax.js',
+      'src/toolkit/util/dialogs.js',
       'src/toolkit/util/file-download.js',
+      'src/toolkit/util/jquery.js',
       'src/toolkit/util/on-document-loaded.js',
       'src/toolkit/util/pangram.js',
+      'src/toolkit/util/print-r.js',
+      'style/dialogs.scss',
+      'style/error-dialogs.scss',
     ],
   }),
 ]);
