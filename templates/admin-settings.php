a8e3ab31f598fe0f556be5e5ca2caf9ef1f7a15c
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-09-17 16:22:50 +0200

    fix(settings): account for changes in AssetsTrait

--- templates/admin-settings.php
+++ templates/admin-settings.php
@@ -20,8 +20,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-script($appName, $assets['js']['asset']);
-style($appName, $assets['css']['asset']);
+script($appName, $assets['js']);
+style($appName, $assets['css']);
 
 ?>
 <div id="<?= $appName ?>-admin-settings"></div>
